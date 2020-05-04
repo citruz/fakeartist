7b87bd535fb9fd303271e1288e0a4e19e05000f2
Felix Seele <fseele@gmail.com>
2020-05-05 00:03:27 +0200

    updated url in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :fakeartist, FakeartistWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  url: [host: "fakeartist.felixseele.de", port: 443],
   check_origin: ["https://${APP_NAME}.gigalixirapp.com", "https://fakeartist.felixseele.de"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 

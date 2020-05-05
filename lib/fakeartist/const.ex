137c83f93c7d34733711b61180f747efbcfe7010
Felix Seele <fseele@gmail.com>
2020-05-05 22:49:19 +0200

    changed color

--- lib/fakeartist/const.ex
+++ lib/fakeartist/const.ex
@@ -1,4 +1,4 @@
 defmodule Fakeartist.Const do
     def wxMAX_PLAYERS, do: 100
-    def wxCOLORS, do: [:black, :blue, :red, :green, :yellow, :purple, :violet]
+    def wxCOLORS, do: [:black, :blue, :red, :green, :chocolate, :purple, :violet, :orange, :pink]
 end
